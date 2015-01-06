2c8b3c58e553e44f0985f88369b273a1ea72caa6
Luca Lanziani <luca@lanziani.com>
2015-01-06 00:01:30 +0000

    Add basic file_attr module test

---
+++ test/file_attr_test.py
@@ -0,0 +1,22 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import unittest
+
+from file_attr import FileAttr, FileNotFoundError
+from nose.tools import raises
+
+TEST_FILE = './test/empty_file.test'
+MD5 = 'd41d8cd98f00b204e9800998ecf8427e',
+SHA1 = 'da39a3ee5e6b4b0d3255bfef95601890afd80709',
+SHA256 = 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+
+
+class FileAttrTest(unittest.TestCase):
+
+    def setUp(self):
+        self.file_attr = FileAttr(TEST_FILE)
+
+    @raises(FileNotFoundError)
+    def raise_if_file_not_exist_test(self):
+        FileAttr(TEST_FILE.replace('test', 'notExists'))

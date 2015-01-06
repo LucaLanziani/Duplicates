865bbbbcadbc307561540ae7a49a77c995bec143
Luca Lanziani <luca@lanziani.com>
2015-01-06 00:07:18 +0000

    Trailing comma on constants are bad ;)

--- test/file_attr_test.py
+++ test/file_attr_test.py
@@ -7,8 +7,8 @@ from file_attr import FileAttr, FileNotFoundError
 from nose.tools import raises
 
 TEST_FILE = './test/empty_file.test'
-MD5 = 'd41d8cd98f00b204e9800998ecf8427e',
-SHA1 = 'da39a3ee5e6b4b0d3255bfef95601890afd80709',
+MD5 = 'd41d8cd98f00b204e9800998ecf8427e'
+SHA1 = 'da39a3ee5e6b4b0d3255bfef95601890afd80709'
 SHA256 = 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
 
 

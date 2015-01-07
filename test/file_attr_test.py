5504ff5239adc0f71b6d801bcc4d13a226a55f20
Luca Lanziani <luca@lanziani.com>
2015-01-07 14:58:45 +0100

    Add extention test

--- test/file_attr_test.py
+++ test/file_attr_test.py
@@ -6,7 +6,7 @@ import unittest
 from file_attr import FileAttr, FileNotFoundError
 from nose.tools import raises, eq_
 
-TEST_FILE = './test/empty_file.test'
+TEST_FILE = './test/empty file.exe.test'
 MD5 = 'd41d8cd98f00b204e9800998ecf8427e'
 SHA1 = 'da39a3ee5e6b4b0d3255bfef95601890afd80709'
 SHA256 = 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
@@ -16,6 +16,9 @@ class FileAttrTest(unittest.TestCase):
 
     def setUp(self):
         self.file_attr = FileAttr(TEST_FILE)
+
+    def tearDown(self):
+        pass
 
     @raises(FileNotFoundError)
     def raise_if_file_not_exist_test(self):
@@ -35,3 +38,6 @@ class FileAttrTest(unittest.TestCase):
 
     def size_test(self):
         eq_(self.file_attr.size, 0)
+
+    def extention_test(self):
+        eq_(self.file_attr.extention, '.test')

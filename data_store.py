de7d5933e72e031c90e175e6c5ea02511d70004d
Luca Lanziani <luca@lanziani.com>
2015-01-08 16:35:30 +0100

    Improve FileStore
    Use constats instead of strings, add the __repr__ method, create
    properties method to access the data's keys

--- data_store.py
+++ data_store.py
@@ -14,11 +14,12 @@ FILESTORE = ".duplicates.json.gz"
 KNOWN_PATHNAMES = 'known_pathnames'
 PATHNAMES_ATTR = 'pathnames_attr'
 HASH_TO_FILES = 'hash_to_files'
-DEFAULT_DATA = {
-    KNOWN_PATHNAMES: [],
-    PATHNAMES_ATTR: {},
-    HASH_TO_FILES: {}
-}
+
+SIZE = 'size'
+LMTIME = 'lmtime'
+HASH = 'hash'
+PATHNAME = 'pathname'
+LAST_UPDATE = 'updated'
 
 
 class FileStore(object):
@@ -28,9 +29,6 @@ class FileStore(object):
         self.base_dir = absolute_path(directory)
         self.store_path = os.path.join(self.base_dir, FILESTORE)
         self.load()
-        self.known_pathnames = self.data[KNOWN_PATHNAMES]
-        self.pathnames_attr = self.data[PATHNAMES_ATTR]
-        self.hash_to_files = self.data[HASH_TO_FILES]
 
     def _from_json(self):
         try:
@@ -39,12 +37,15 @@ class FileStore(object):
 
             return result
         except Exception:
-            return DEFAULT_DATA
+            return {
+                KNOWN_PATHNAMES: [],
+                PATHNAMES_ATTR: {},
+                HASH_TO_FILES: {},
+            }
 
     def _to_json(self):
-        self.data[KNOWN_PATHNAMES] = list(self.known_pathnames)
         with gzip.open(self.store_path, 'wb') as fd:
-            json.dump(self.data, fd, indent=4)
+            json.dump(self._data, fd, indent=4)
 
     def _rel_path(self, pathname):
         if not os.path.isabs(pathname):
@@ -52,9 +53,6 @@ class FileStore(object):
         return pathname.replace(self.base_dir, '.')
 
     def _relative_data(self, file_attr):
-        """
-        Return the file data relative to the data ` directory
-        """
         rel_pathname = self._rel_path(file_attr.pathname)
         rel_pathname_hash = FileAttr.hash_pathname(rel_pathname)
         return rel_pathname, rel_pathname_hash
@@ -63,10 +61,10 @@ class FileStore(object):
         rel_pathname, rel_pathname_hash = self._relative_data(file_attr)
         self.hash_to_files[file_attr.hash].append(rel_pathname)
         self.pathnames_attr[rel_pathname_hash] = {
-            'size': file_attr.size,
-            'lmtime': file_attr.lmtime,
-            'hash': file_attr.hash,
-            'pathname': rel_pathname
+            SIZE: file_attr.size,
+            LMTIME: file_attr.lmtime,
+            HASH: file_attr.hash,
+            PATHNAME: rel_pathname
         }
         self.known_pathnames.add(rel_pathname_hash)
 
@@ -81,20 +79,33 @@ class FileStore(object):
             return False
         else:
             stored_attr = self.pathnames_attr[pathname_hash]
-            diff_size = stored_attr['size'] != file_attr.size
-            diff_time = stored_attr['lmtime'] != file_attr.lmtime
+            diff_size = stored_attr[SIZE] != file_attr.size
+            diff_time = stored_attr[LMTIME] != file_attr.lmtime
             if diff_size or diff_time:
                 self.remove_pathname(rel_pathname)
                 return False
             return True
 
+    @property
+    def known_pathnames(self):
+        return self._data[KNOWN_PATHNAMES]
+
+    @property
+    def pathnames_attr(self):
+        return self._data[PATHNAMES_ATTR]
+
+    @property
+    def hash_to_files(self):
+        return self._data[HASH_TO_FILES]
+
     def load(self):
-        loaded = self._from_json()
-        loaded[HASH_TO_FILES] = defaultdict(list, loaded[HASH_TO_FILES])
-        loaded[KNOWN_PATHNAMES] = set(loaded[KNOWN_PATHNAMES])
-        self.data = loaded
+        data = self._from_json()
+        data[HASH_TO_FILES] = defaultdict(list, data[HASH_TO_FILES])
+        data[KNOWN_PATHNAMES] = set(data[KNOWN_PATHNAMES])
+        self._data = data
 
     def save(self):
+        self._data[KNOWN_PATHNAMES] = list(self.known_pathnames)
         self._to_json()
 
     def remove_pathname(self, pathname):
@@ -108,3 +119,6 @@ class FileStore(object):
         for _, paths in self.hash_to_files.iteritems():
             if len(paths) > 1:
                 yield map(lambda path: absolute_path(path), paths)
+
+    def __repr__(self):
+        return repr(self._data)

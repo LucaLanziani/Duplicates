89fabc8be4cec0a42f5e4ad5f337af4b66305db0
Luca Lanziani <luca@lanziani.com>
2015-01-15 16:18:28 +0100

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='Duplicates',
+      version='1.0',
+      description='Find duplicate files in a directory',
+      author='Luca Lanziani',
+      author_email='luca@lanziani.com',
+      url='https://github.com/Nss/Duplicates',
+      packages=['duplicates'],
+     )

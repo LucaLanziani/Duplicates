3a682d8a70f1bb33ce4feb704fcb5e50dfd7bfcb
Luca Lanziani <luca@lanziani.com>
2015-01-18 22:29:39 +0100

    Improve commandline interface

--- duplicates/main.py
+++ duplicates/main.py
@@ -1,20 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Usage: %(name)s [options] DIRECTORY [PATTERNS...]
-
-You can filter the analyzed files passing multiple patterns through command
-line, the patterns can include "Unix shell-style wildcards"
-
-Options:
-    --first_n N       stop after N files [default: inf]
-    --verbose         print status update in console
-    --no-store        do not save the gathered information on filesystem
-
-Examples:
-    %(name)s .                  # every file
-    %(name)s . '*.png'          # only .png files
-    %(name)s . '*.png' '*.jpg'  # .png and .jpg files
-"""
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
@@ -26,24 +11,24 @@ from duplicates.directory import Directory
 from duplicates.file_attr import FileAttrFactory
 from duplicates.filters import UnixShellWildcardsFilter
 from duplicates.output import ConsoleOutput, DummyOutput
-from schema import And, Optional, Or, Schema, SchemaError, Use
+from schema import And, Optional, Schema, SchemaError
 
 
 class Duplicates():
 
-    def __init__(self, opt):
-        self._parse_opt(opt)
+    def __init__(self, directory, verbose=False, unix_patterns=None):
+        self._settings(verbose, unix_patterns)
+        self._directory = Directory(directory)
         self._store = FileStore(self._directory.pathname)
         self._pathname_sha_cache = {}
 
-    def _parse_opt(self, opt):
+    def _settings(self, verbose, unix_patterns):
         self.output = DummyOutput()
-        if opt["--verbose"]:
+        if verbose:
             self.output = ConsoleOutput()
-        self._unixpatterns_filter = UnixShellWildcardsFilter(opt['PATTERNS'])
-        self._first_n = float(opt['--first_n'])
-        self._directory = Directory(opt['DIRECTORY'])
-        self._allow_save = not opt['--no-store']
+        if not unix_patterns:
+            unix_patterns = ['*']
+        self._unixpatterns_filter = UnixShellWildcardsFilter(unix_patterns)
 
     def _print_state(self, signum, stack):
         analized = len(self._store.known_pathnames)
@@ -64,55 +49,81 @@ class Duplicates():
 
     def _file_list(self):
         content = self._directory.dir_content(
-            pathname_filter=self._valid_pathname,
-            limit=self._first_n
+            pathname_filter=self._valid_pathname
         )
         for pathname in content:
             yield pathname
 
     def collect_data(self):
+        """
+        Collect files data and return the store object
+        """
         for pathname in self._file_list():
             self._store.add_file(FileAttrFactory.by_pathname(pathname))
             self._print_state(None, None)
         self.output.print()
+        return self._store
 
     def _initialize(self):
         self._file_number()
 
     def _terminate(self):
-        if self._allow_save:
-            self._store.save()
+        pass
 
-    def run(self):
+    def run(self, store=True):
         self._initialize()
         self.collect_data()
+        if store:
+            self._store.save()
         self._terminate()
 
 
-def validate_args(opt):
-    schema = Schema({
-        'DIRECTORY': And(os.path.exists, error="DIRECTORY does not exists"),
-        '--first_n': Or(u'inf', And(Use(int)),
-                        error="--first_n=N should be integer"),
-        Optional('--verbose'): bool,
-        Optional('--no-store'): bool,
-        Optional('PATTERNS'): [str]
-    })
-    try:
-        opt = schema.validate(opt)
-    except SchemaError as e:
-        exit(e)
-    return opt
+class CommandLineInterface(object):
+    """Usage: %(name)s [options] DIRECTORY [PATTERNS...]
 
+    You can filter the analyzed files passing multiple patterns through command
+    line, the patterns can include "Unix shell-style wildcards"
 
-def run(name=None):
-    if name is None:
-        name = __file__
-    opt = docopt(__doc__ % {'name': name}, argv=None, help=True,
-                 version=None, options_first=False)
-    opt = validate_args(opt)
-    Duplicates(opt).run()
+    Options:
+        --verbose         print status update in console
+        --no-store        do not save the gathered information on filesystem
 
+    Examples:
+        %(name)s .                  # every file
+        %(name)s . '*.png'          # only .png files
+        %(name)s . '*.png' '*.jpg'  # .png and .jpg files
+    """
+
+    def __init__(self):
+        super(CommandLineInterface, self).__init__()
+
+    def _validate_args(self, opt):
+        schema = Schema({
+            'DIRECTORY': And(os.path.exists, error="Dir does not exists"),
+            Optional('--verbose'): bool,
+            Optional('--no-store'): bool,
+            Optional('PATTERNS'): [str]
+        })
+        try:
+            opt = schema.validate(opt)
+        except SchemaError as e:
+            exit(e)
+        return opt
+
+    def _parse_args(self, name=None):
+        if name is None:
+            name = __file__
+        opt = docopt(self.__doc__ % {'name': name}, argv=None, help=True,
+                     version=None, options_first=False)
+        return self._validate_args(opt)
+
+    def run(self, name=None):
+        opt = self._parse_args(name)
+        Duplicates(
+            opt['DIRECTORY'],
+            verbose=opt['--verbose'],
+            unix_patterns=opt['PATTERNS']
+        ).run(not opt['--no-store'])
 
 if __name__ == '__main__':
-    run()
+    CommandLineInterface().run()

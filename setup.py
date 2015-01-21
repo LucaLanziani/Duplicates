8c7311ab13c3aa49d531852b7e80f359c0f63e15
Luca Lanziani <luca@lanziani.com>
2015-01-21 19:41:17 +0100

    Add dependencies and change the name to downcase

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@
 from distutils.core import setup
 
 setup(
-    name='Duplicates',
+    name='duplicates',
     version='1.0',
     description='Find duplicate files in a directory',
     author='Luca Lanziani',
     author_email='luca@lanziani.com',
     url='https://github.com/Nss/Duplicates',
+    install_requires=['docopt', 'schema'],
     packages=['duplicates'],
+    scripts=['bin/duplicates'],
 )

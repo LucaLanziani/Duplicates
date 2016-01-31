bfab2589efcc7c75358b1183af128dbaa68ddb39
Luca Lanziani <luca@lanziani.com>
2016-01-31 13:08:39 +0100

    Test index store and remove unneeded code

--- test/indexer_test.py
+++ test/indexer_test.py
@@ -2,17 +2,23 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import unittest
+import os
 
 from duplicates.indexer import Indexer
-from nose.tools import eq_
+from duplicates.store.json_store import FILESTORE
+from nose.tools import eq_, assert_true
 
 TEST_DIR = './test/files/test_dir_1/'
-
+STORE_PATH = os.path.join(TEST_DIR, FILESTORE)
 
 class IndexerTest(unittest.TestCase):
 
     def setUp(self):
         self.indexer = Indexer(TEST_DIR)
+
+    def tearDown(self):
+        if os.path.isfile(STORE_PATH):
+            os.unlink(STORE_PATH)
 
     def test_collect_data(self):
         result = self.indexer.index()
@@ -38,3 +44,7 @@ class IndexerTest(unittest.TestCase):
         indexer.run(store=False)
         result = indexer.index()
         eq_(len(result), 2)
+
+    def test_store(self):
+        self.indexer.run()
+        assert_true(os.path.isfile(STORE_PATH))
